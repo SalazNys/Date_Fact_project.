6b5142a8c895a455abe3e6e191d58dec81b2c17e
Salazar <salaznys@gmail.com>
2017-08-03 21:26:08 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function getRandommoment() {
-  const moments = ["moment", "date", "time", "parse", "format", "validate", "i18n", "l10n", "ender"];
-  return moments[math.floor(math.random() * moment.length)]
-
-}
-
-module.exports = getRandommoment;
